d4814c240e7d11558699c5f8cfb4b525f411a187
Demario-Huang <119010108@link.cuhk.edu.cn>
2023-11-03 18:07:11 -0500

    add the Store and Load

--- testgen.py
+++ testgen.py
@@ -31,7 +31,23 @@ def IShift_Gen(shamt, src1, funct3, dst, opcode, funct7):
   inst = str(funct7) + shamt_binary + src1_binary + str(funct3) + dst_binary + str(opcode)
   return int(inst, 2)
 
+def Store_Gen(src2, src1, imm, funct3, opcode):
+  src1_binary = reg2binary(src1)
+  src2_binary = reg2binary(src2)
+  if (imm < 0):
+    imm = 2**12 + imm 
+  imm_binary = format(imm, '012b')
+  inst = imm_binary[5:12]+ src2_binary + src1_binary + str(funct3) + imm_binary[0:5] + str(opcode)
+  return int(inst, 2)
 
+def Load_Gen(src1, imm, funct3, dst, opcode):
+  src1_binary = reg2binary(src1)
+  dst_binary = reg2binary(dst)
+  if (imm < 0):
+    imm = 2**12 + imm 
+  imm_binary = format(imm, '012b')
+  inst = imm_binary + src1_binary + str(funct3) + dst_binary + str(opcode)
+  return int(inst, 2)
 
 
 # return a 32 bit binary instruction
@@ -70,10 +86,41 @@ def Srli(dst, src1, shamt):
   inst = IShift_Gen(shamt, src1, funct3, dst, opcode, funct7)
   return inst
 
-def Store(src2, src1, imm):
+def Slli(dst, src1, shamt):
+  opcode = '0010011'
+  funct3 = '001'
+  funct7 = '0000000'
+  inst = IShift_Gen(shamt, src1, funct3, dst, opcode, funct7)
+  return inst
+
+def StoreW(src2, src1, offset):
   opcode = '0100011'
   funct3 = '010'
-  inst = Itype_Gen(imm, src1, funct3, src2, opcode)
+  inst = Store_Gen(src2, src1, offset, funct3, opcode)
+  return inst
+
+def StoreB(src2, src1, offset):
+  opcode = '0100011'
+  funct3 = '000'
+  inst = Store_Gen(src2, src1, offset, funct3, opcode)
+  return inst
+
+def StoreH(src2, src1, offset):
+  opcode = '0100011'
+  funct3 = '001'
+  inst = Store_Gen(src2, src1, offset, funct3, opcode)
+  return inst
+
+def LoadB(dst, src1, offset):
+  opcode = '0000011'
+  funct3 = '000'
+  inst = Load_Gen(src1, offset, funct3, dst, opcode)
+  return inst
+
+def LoadW(dst, src1, offset):
+  opcode = '0000011'
+  funct3 = '010'
+  inst = Load_Gen(src1, offset, funct3, dst, opcode)
   return inst
 
 def Halt():
@@ -96,8 +143,9 @@ def Instruction_Gen():
   instructions = []
   ### write the assembly code here to generate the machine code
   instructions.append(Add(3, 1, 2))
-  instructions.append(Addi(4, 1, -1))
-  instructions.append(Srli(4, 4, 2))
+  # instructions.append(StoreB(3, 0, 1024))
+  instructions.append(StoreB(3, 0, 1024))
+  instructions.append(LoadB(4, 0, 1))
   instructions.append(Halt())
 
   return instructions
@@ -110,9 +158,9 @@ def write_mem_in(filename, instructions):
             f.write(f"{(inst & 0x0000FF00) >> 8:02x}\n")
             f.write(f"{(inst & 0x00FF0000) >> 16:02x}\n")
             f.write(f"{(inst & 0xFF000000) >> 24:02x}\n")
-        remaining = 1024 - len(instructions)
+        remaining = 1025 - len(instructions) * 4
         for i in range(remaining):
-            f.write("00\n00\n00\n00\n")
+            f.write("00\n")
 
 def write_reg_in(filename, registers):
     with open(filename, 'w') as f:
